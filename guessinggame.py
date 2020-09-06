854548a6a75b1700c15fe5fc27dcee99627ad571
brgotcher <brgotcher@gmail.com>
2020-09-06 07:17:31 -0500

    edit guessinggame.py, add whileloop.py and adventure.py

--- guessinggame.py
+++ guessinggame.py
@@ -1,47 +1,23 @@
-answer = 5
+import random
 
-print("Please guess number between 1 and 10: ")
-guess = int(input())
+highest = 10
+answer = random.randint(1, highest)
+print(answer)   # TODO: remove after testing
 
-if guess == answer:
-    print("You got it first time")
-else:
-    if guess < answer:
-        print("Please guess higher")
-    else:   # guess must be greater than answer
-        print("Please guess lower")
+guess = ""
+print("Please guess number between 1 and {}: ".format(highest))
+while guess != answer:
     guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
+    if guess == 0:
+        print("quitter!")
+        break
+    elif guess == answer:
+        print("Correct!")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
+        if guess < answer:
+            print("Please guess higher")
+        else:   # guess must be greater than answer
+            print("Please guess lower")
 
-# if guess != answer:
-#     if guess < answer:
-#         print("Please guess higher")
-#     else:   # guess must be greater than answer
-#         print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# else:
-#     print("You got it first time")
 
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# else:
-#     print("You got it first time")
